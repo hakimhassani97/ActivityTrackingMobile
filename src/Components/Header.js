4137559eeba89dcb5c2a41ddcffc3b7d6aff26e6
hakimhassani97 <hakimhassani97@gmail.com>
2021-02-16 00:35:42 +0100

    added header

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header(){
+    return (
+        <div style={{height:'50px', alignItems:'center', flexDirection:'row', display:'flex', backgroundColor:'InfoBackground', justifyContent:'center', fontWeight:'bolder'}}>
+            Suivi des personnes agées
+        </div>
+    )
+}
