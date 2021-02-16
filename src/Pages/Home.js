c9d5cfddd4d7eb22499ca23ae12b8a43621dbda4
hakimhassani97 <hakimhassani97@gmail.com>
2021-02-16 02:00:23 +0100

    added firebase

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import '../App.css';
+import 'firebase'
 
-function Home() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <button style={{ backgroundColor: 'InfoBackground', height: '100px', borderRadius: '50%', fontWeight: 'bolder', boxShadow: '0 0 solid black' }}>
-                    Gyroscope
-                </button>
-            </header>
-        </div>
-    );
+class Home extends React.Component {
+    constructor(props){
+        super(props)
+    }
+    render (){
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <button style={{ backgroundColor: 'InfoBackground', height: '100px', border:'0', borderRadius: '50%', fontWeight: 'bolder', boxShadow: '0 0 solid black' }}>
+                        Gyroscope
+                    </button>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default Home;
