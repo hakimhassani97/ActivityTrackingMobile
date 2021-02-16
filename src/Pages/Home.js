003d4c3af470cb2cdf0808efd2d0537aea21a609
hakimhassani97 <hakimhassani97@gmail.com>
2021-02-16 01:41:43 +0100

    update

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,13 +2,15 @@ import React from 'react';
 import '../App.css';
 
 function Home() {
-  return (
-    <div className="App">
-      <header className="App-header">
-          <button style={{backgroundColor:'InfoBackground', height:'100px', borderRadius:'50%', fontWeight:'bolder', boxShadow:'0 0 solid black'}}>Commencer</button>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <button style={{ backgroundColor: 'InfoBackground', height: '100px', borderRadius: '50%', fontWeight: 'bolder', boxShadow: '0 0 solid black' }}>
+                    Gyroscope
+                </button>
+            </header>
+        </div>
+    );
 }
 
 export default Home;
