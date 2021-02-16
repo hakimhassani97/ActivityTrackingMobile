b6d21ae0803461ccc4db6949c0d0e95d4b4eb580
hakimhassani97 <hakimhassani97@gmail.com>
2021-02-16 02:38:51 +0100

    added accel

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import '../App.css';
-import 'firebase'
+import {db} from '../index'
 
 class Home extends React.Component {
+    startAccel = ()=>{
+        window.addEventListener('deviceorientation', e =>
+            console.log(e.alpha, e.beta, e.gamma)
+        );
+
+        let k = db.ref('users/').push().key
+        db.ref('users/'+k).set({
+            username: 'hakimhassani97',
+            email: 'hakimhassani97@gmail.com',
+            gyro: Math.random()
+        });
+        
+    }
     render (){
         return (
             <div className="App">
                 <header className="App-header">
-                    <button style={{ backgroundColor: 'InfoBackground', height: '100px', border:'0', borderRadius: '50%', fontWeight: 'bolder', boxShadow: '0 0 solid black' }}>
+                    <button style={{ backgroundColor: 'InfoBackground', height: '100px', border:'0', borderRadius: '50%', fontWeight: 'bolder', boxShadow: '0 0 solid black' }}
+                        onClick={this.startAccel}>
                         Start Gyroscope
                     </button>
                 </header>
--- src/index.js
+++ src/index.js
@@ -6,14 +6,13 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 // Firebase App (the core Firebase SDK) is always required and must be listed first
-import firebase from "firebase/app";
+import firebase from "firebase";
 // If you enabled Analytics in your project, add the Firebase SDK for Analytics
 import "firebase/analytics";
 // Add the Firebase products that you want to use
 import "firebase/auth";
 import "firebase/firestore";
 // TODO: Replace the following with your app's Firebase project configuration
-// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
 // For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
 var firebaseConfig = {
   apiKey: "AIzaSyBhWCedCUO7yVNDd-nhnAXatyq0V6GShic",
@@ -27,7 +26,6 @@ var firebaseConfig = {
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// firebase.analytics();
 
 
 ReactDOM.render(
@@ -46,4 +44,5 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-export default firebase;
+
+export const db = firebase.database()
