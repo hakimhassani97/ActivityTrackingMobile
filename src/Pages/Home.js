f92f220272d56a56b0cb8eeca4083bb0d061e316
hakimhassani97 <hakimhassani97@gmail.com>
2021-02-16 02:08:35 +0100

    updated

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -3,15 +3,12 @@ import '../App.css';
 import 'firebase'
 
 class Home extends React.Component {
-    constructor(props){
-        super(props)
-    }
     render (){
         return (
             <div className="App">
                 <header className="App-header">
                     <button style={{ backgroundColor: 'InfoBackground', height: '100px', border:'0', borderRadius: '50%', fontWeight: 'bolder', boxShadow: '0 0 solid black' }}>
-                        Gyroscope
+                        Start Gyroscope
                     </button>
                 </header>
             </div>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,31 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/firestore";
+// TODO: Replace the following with your app's Firebase project configuration
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+var firebaseConfig = {
+  apiKey: "AIzaSyBhWCedCUO7yVNDd-nhnAXatyq0V6GShic",
+  authDomain: "elderly-622a9.firebaseapp.com",
+  databaseURL: "https://elderly-622a9-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "elderly-622a9",
+  storageBucket: "elderly-622a9.appspot.com",
+  messagingSenderId: "792971798543",
+  appId: "1:792971798543:web:2c5ffefb25ca6926131b7a",
+  measurementId: "G-GXN13DRQ8W"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+// firebase.analytics();
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -21,3 +46,4 @@ serviceWorkerRegistration.register();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+export default firebase;
