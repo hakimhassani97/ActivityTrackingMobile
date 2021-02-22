ded502d1dad9f91539dd80177b7dda8b95a5af25
hakimhassani97 <hakimhassani97@gmail.com>
2021-02-22 12:02:04 +0100

    added routes

---
+++ src/Constantes/Routes.js
@@ -0,0 +1,8 @@
+import {base_url} from '../../package.json'
+
+let Routes = {
+    home: base_url+'/Home',
+    profile: base_url+'/Profile',
+    notifications: base_url+'/Notifications'
+}
+export default Routes
