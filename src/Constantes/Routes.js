9c3763768b3fc4b71fe064cd15b442abe0225582
hakimhassani97 <hakimhassani97@gmail.com>
2021-02-23 08:48:43 +0100

    updated routes

--- src/Constantes/Routes.js
+++ src/Constantes/Routes.js
@@ -1,6 +1,7 @@
 import {base_url} from '../../package.json'
 
 let Routes = {
+    base: base_url,
     home: base_url+'/Home',
     profile: base_url+'/Profile',
     notifications: base_url+'/Notifications'
