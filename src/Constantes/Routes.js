bb8b4d8b1855012e650fa4932b0cc64c1d1caa14
hakimhassani97 <hakimhassani97@gmail.com>
2021-03-06 19:44:51 +0100

    added auth

--- src/Constantes/Routes.js
+++ src/Constantes/Routes.js
@@ -1,9 +1,14 @@
 import {base_url} from '../../package.json'
 
+let base_mobile = base_url + '/' + 'mobile'
+let base_web = base_url + '/' + 'web'
+
 let Routes = {
-    base: base_url,
-    home: base_url+'/Home',
-    profile: base_url+'/Profile',
-    notifications: base_url+'/Notifications'
+    base: base_mobile,
+    home: base_mobile+'/Home',
+    profile: base_mobile+'/Profile',
+    login: base_mobile+'/Login',
+    register: base_mobile+'/Register',
+    notifications: base_mobile+'/Notifications'
 }
 export default Routes
--- src/Helpers/Auth.js
+++ src/Helpers/Auth.js
@@ -1,3 +1,5 @@
+import { Redirect, Route } from "react-router"
+import Routes from "../Constantes/Routes"
 
 const initAuth = (db)=>{
     let uid = localStorage.getItem('uid')
@@ -7,5 +9,49 @@ const initAuth = (db)=>{
     }
     return uid
 }
+
+export const login = (user)=>{
+    localStorage.setItem('currentUser', JSON.stringify(user))
+}
+export const logout = ()=>{
+    localStorage.removeItem('currentUser')
+}
+
+export const isLoggedIn = ()=>{
+    let user = localStorage.getItem('currentUser')
+    user = JSON.parse(user)
+    return user
+}
+
+/**
+ * the PrivateRoute Component
+ * redirects to login if not authenticated
+ */
+ export const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => {
+        if (!isLoggedIn()) {
+            // user not authenticated
+            return <Redirect to={{ pathname: Routes.login, state: { from: props.location } }} />
+        }
+        // user authenticated
+        return <Component {...props} />
+    }} />
+)
+
+/**
+ * the PublicRoute Component
+ * redirects to home if authenticated
+ */
+export const PublicRoute = ({ component: Component, ...rest }) => (
+    <Route {...rest} render={props => {
+        if (isLoggedIn()) {
+            // user authenticated
+            return <Redirect to={{ pathname: Routes.home, state: { from: props.location } }} />
+        }
+        // user not authenticated
+        return <Component {...props} />
+    }} />
+)
+
 
 export {initAuth}
